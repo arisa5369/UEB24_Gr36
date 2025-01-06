3158e23c8b4f3623ad8fc541b6fcf13a4fbd260f
arisa5369 <arisadragusha5@gmail.com>
2025-01-06 23:33:17 +0100

    lidhja me links

--- adopt/footer.js
+++ adopt/footer.js
@@ -1,4 +1,4 @@
-fetch('../adopt/footer.html')
+fetch('../../adopt/footer.html')
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok ' + response.statusText);
